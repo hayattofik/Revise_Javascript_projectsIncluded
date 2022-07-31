f8132a63b6a68959312bf63ee41427d1d4488cd0
hayat <hayattofik22@gmail.com>
2022-07-31 12:40:25 +0300

    Functions

---
+++ Functions/script.js
@@ -0,0 +1,38 @@
+/* 
+  TODO: Write a function that sums two numbers
+  TODO: Write a function that subtracts two numbers
+  TODO: Write a function that divides two numbers.
+  TODO: Write a function that multiplies two numbers.
+  *NOTE* be sure to handle dividing by zero 😉
+  ES5 Syntax: function Add(){}
+  ES6 Syntax (Arrow function): const add = () => {}
+*/
+
+function add(a,b){
+    //Add function here
+    return a + b
+  }
+  
+  function sub(a,b){
+    //Subtract function here
+    return a-b
+  }
+  
+  function div(a,b){
+    
+    //Divide function here
+    return a/b
+  }
+  
+  function mul(a,b){
+    //Multiply function here
+    return a * b
+  }
+  
+  console.log('hello from the SUM exercise')
+  /* 
+    TODO: create a function that console logs the result of any of the above operations.
+  */
+  const printFun = (functions) => console.log(functions)
+  printFun(add(3,2))
+  printFun(mul(3,2))
